d9c8362ea90bcfff21b7dd667ffbca366eacd509
Min A Cho Zeno <minachozeno@Mins-Laptop.local>
2024-12-01 09:48:47 -0600

    2024 Day 1

---
+++ 2024/1_p1.py
@@ -0,0 +1,17 @@
+## Part 1
+day1lines = [line.rstrip() for line in open('1_p1.txt')]
+list1 = [int(nums.split('   ')[0]) for nums in day1lines]
+list2 = [int(nums.split('   ')[1]) for nums in day1lines]
+
+list1.sort()
+list2.sort()
+
+diffs = [abs(list2[i] - list1[i]) for i in range(len(list1))]
+
+print(sum(diffs))
+
+
+## Part 2
+counts_multiplied = [list2.count(num) * num for num in list1]
+
+print(sum(counts_multiplied))
