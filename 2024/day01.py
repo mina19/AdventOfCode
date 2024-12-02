e61d1bcd82edefb170762327bc1668f6cbf21571
MCZeno <MCZeno@eprod.com>
2024-12-02 14:56:24 +0000

    Clean up code

--- 2024/day01.py
+++ 2024/day01.py
@@ -5,18 +5,16 @@ save_data(2024, 1)
 
 ## Part 1
 lines = [line.rstrip() for line in open("day01.txt")]
-list1 = [int(nums.split("   ")[0]) for nums in lines]
-list2 = [int(nums.split("   ")[1]) for nums in lines]
+list1 = [int(nums.split()[0]) for nums in lines]
+list2 = [int(nums.split()[1]) for nums in lines]
 
 list1.sort()
 list2.sort()
 
 diffs = [abs(list2[i] - list1[i]) for i in range(len(list1))]
-
 print(sum(diffs))
 
 
 ## Part 2
 counts_multiplied = [list2.count(num) * num for num in list1]
-
 print(sum(counts_multiplied))
