e5cb4de1c8983c71837fe6cb1f549886e33bc7cf
MCZeno <MCZeno@eprod.com>
2024-12-03 13:58:28 +0000

    Save work

--- 2024/day03.py
+++ 2024/day03.py
@@ -31,35 +31,65 @@ def part1():
 print(part1())
 
 # Part 2
-match_positions = [match.start() for match in re.finditer(pattern, mystr)]
-mul_dict = dict(zip(match_positions, matches))
-
 do_pattern = r"do\(\)"
 dont_pattern = r"don't\(\)"
 do_matches = [(match.start(), "do") for match in re.finditer(do_pattern, mystr)]
 dont_matches = [(match.start(), "dont") for match in re.finditer(dont_pattern, mystr)]
 
 combined_matches = do_matches + dont_matches
-sorted_combined_matches = sorted(combined_matches, key=lambda x: x[0])
+sorted_combined_matches = sorted(combined_matches, key=lambda x: x[0], reverse=True)
 
 
 def find_last_instruction(target):
-    for t in reversed(sorted_combined_matches):
+    for t in sorted_combined_matches:
         if t[0] < target:
             return t
     return (0, "do")
 
 
+match_positions = [match.start() for match in re.finditer(pattern, mystr)]
+mul_dict = dict(zip(match_positions, matches))
+
+
+## This is slower than I'd like.
 @timeit
-def part2():
+def part2_slow():
     prod = 0
-    for i in range(len(mystr)):
-        if i in match_positions:
-            match = mul_dict[i]
-            instruction = find_last_instruction(i)[1]
-            if instruction == "do":
-                prod += int(match[0]) * int(match[1])
+    for i in match_positions:
+        match = mul_dict[i]
+        instruction = find_last_instruction(i)
+        if instruction[1] == "do":
+            prod += int(match[0]) * int(match[1])
     return prod
 
 
-print(part2())
+print(part2_slow())
+
+
+## Faster version
+@timeit
+def part2_fast():
+    match_indices = re.finditer(pattern, mystr)
+
+    do = True
+    total = 0
+    border = 0
+
+    for match in match_indices:
+        dont_ind = mystr[border : match.start()].find("don't()")
+        do_ind = mystr[border : match.start()].find("do()")
+
+        if dont_ind != -1 and dont_ind > do_ind:
+            do = False
+        elif do_ind != -1 and do_ind > dont_ind:
+            do = True
+
+        if do:
+            total += int(match.group(1)) * int(match.group(2))
+
+        border = match.end()
+
+    return total
+
+
+print(part2_fast())
