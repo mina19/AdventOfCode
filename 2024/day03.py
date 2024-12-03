f41a79ca4cdfc6201782d3994f010d7e68b49210
MCZeno <MCZeno@eprod.com>
2024-12-03 12:50:19 +0000

    Save part 1 day 3

--- 2024/day03.py
+++ 2024/day03.py
@@ -1,4 +1,6 @@
 ## Pull Data
+import re
+
 from get_data import save_data
 
 day = 3
@@ -6,3 +8,17 @@ save_data(2024, day)
 
 ## Part 1
 lines = [line.rstrip() for line in open(f"day{day:02d}.txt")]
+
+mystr = ""
+for line in lines:
+    mystr += line
+
+pattern = r"mul\((\d+),(\d+)\)"
+
+matches = re.findall(pattern, mystr)
+
+prod = 0
+for num1, num2 in matches:
+    prod += int(num1) * int(num2)
+
+print(prod)
