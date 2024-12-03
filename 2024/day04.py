43fa0513f546420b53acdf1c5f05f318a81fa49b
MCZeno <MCZeno@eprod.com>
2024-12-03 14:57:19 +0000

    Clean up file

--- 2024/day04.py
+++ 2024/day04.py
@@ -4,6 +4,6 @@ from pathlib import Path
 from get_data import save_data, timeit
 
 save_data(2024, day := 4)
+data = Path(f"day{day:02d}.txt").read_text()
 
 ## Part 1
-data = Path(f"day{day:02d}.txt").read_text()
