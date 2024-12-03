42c165954b7befa380088c4f1fe5ad79d1497995
MCZeno <MCZeno@eprod.com>
2024-12-03 14:58:51 +0000

    Save template

--- 2024/template.py
+++ 2024/template.py
@@ -3,8 +3,24 @@ from pathlib import Path
 
 from get_data import save_data, timeit
 
-day = 1
-save_data(2024, day)
+save_data(2024, day := 1)
+data = Path(f"day{day:02d}.txt").read_text()
 
 ## Part 1
-data = Path(f"day{day:02d}.txt").read_text()
+
+
+@timeit
+def part1():
+    pass
+
+
+part1()
+## Part 2
+
+
+@timeit
+def part2():
+    pass
+
+
+part2()
