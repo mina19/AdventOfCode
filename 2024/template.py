bc056f592fea887f21f75d636cce74e0a52d47cf
Min A Cho Zeno <minacho19@gmail.com>
2024-12-08 12:09:07 -0600

    Update template

--- 2024/template.py
+++ 2024/template.py
@@ -4,8 +4,8 @@ from pathlib import Path
 from get_data import save_data, timeit
 
 save_data(2024, day := 1)
-data = Path(f"day{day:02d}.txt").read_text()
 data = Path(f"2024/{day}/day{day:02d}.txt").read_text()
+data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text()
 
 
 ## Part 1
