e52459f0526737241e5f6b323164b43a0019de58
MCZeno <MCZeno@eprod.com>
2024-12-09 14:55:27 +0000

    Save work

--- 2024/9/day09.py
+++ 2024/9/day09.py
@@ -5,73 +5,85 @@ from get_data import save_data, timeit
 
 save_data(2024, day := 9)
 data = Path(f"2024/{day}/day{day:02d}.txt").read_text().splitlines()[0]
-# data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()[0]
+data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()[0]
 
 # The digits alternate between indicating the length of a file and the length of free space.
-# If it's a free space, set second number to -1
-# If it's a file, set the second number to the file ID
+# Diskmap_table is a list of tuples where the first number is the length of the entry
+# The second number is the file ID if it's a file or -1 if it's a free space/gap
 diskmap_table = [(int(num), i // 2 if i % 2 == 0 else -1) for i, num in enumerate(data)]
 
 
 # Part 1
 @timeit
 def part1():
+    print(diskmap_table)
     while True:
         # Find the first gap
-        gap_index = -1
+        table_gap_index = -1
         for i, diskmap_tuple in enumerate(diskmap_table):
-            # Entry with a -1 is a gap because it's a free space
+            # If the tuple has a -1 in the second position, it's a gap
             if diskmap_tuple[1] == -1:
-                gap_index = i
+                table_gap_index = i
                 break
-        if gap_index == -1:
-            break  # No gaps found so exit while loop
-        gap_tuple = diskmap_table[gap_index]
+        gap_tuple = diskmap_table[table_gap_index]
 
         # Second part: Find the next file after the gap
-        file_index = -1
-        for i in range(gap_index + 1, len(diskmap_table)):
+        table_file_index = -1
+        for i in range(table_gap_index + 1, len(diskmap_table)):
+            # Keep iterating until finding the last file after the gap
             diskmap_tuple = diskmap_table[i]
             if diskmap_tuple[1] != -1:
-                file_index = i
-        if file_index == -1:
+                table_file_index = i
+        if table_file_index == -1:
             break  # No files after gap so exit while loop
-        file_tuple = diskmap_table[file_index]
+        file_tuple = diskmap_table[table_file_index]
 
         gap_length, gap_id = gap_tuple  # gap_id is always -1 by definition of a gap
         file_length, file_id = file_tuple
 
+        # Rearrange the diskmap_table
         if file_length > gap_length:
-            diskmap_table[gap_index] = (gap_length, file_id)
-            diskmap_table[file_index] = (file_length - gap_length, file_id)
-            diskmap_table.insert(file_index + 1, (gap_length, gap_id))
+            # Since the file length is bigger than the gap length,
+            # move part of the file to the gap
+            diskmap_table[table_gap_index] = (gap_length, file_id)
+            # The remaining part of the file still remains where it was
+            diskmap_table[table_file_index] = (file_length - gap_length, file_id)
+            # Because the file length was larger than the gap length, we need to track
+            # the original gap in the diskmap_table
+            diskmap_table.insert(table_file_index + 1, (gap_length, gap_id))
         elif file_length == gap_length:
-            diskmap_table[gap_index] = (gap_length, file_id)
-            diskmap_table[file_index] = (file_length, gap_id)
+            # Swap the file and the gap
+            diskmap_table[table_gap_index] = (file_length, file_id)
+            diskmap_table[table_file_index] = (file_length, gap_id)
         else:
-            diskmap_table[gap_index] = (file_length, file_id)
-            diskmap_table[file_index] = (file_length, gap_id)
-            diskmap_table.insert(gap_index + 1, (gap_length - file_length, gap_id))
-        # print([el[1] for el in diskmap_table])
+            # Move the file to the gap
+            diskmap_table[table_gap_index] = (file_length, file_id)
+            diskmap_table[table_file_index] = (file_length, gap_id)
+            # Because the gap length was larger than the file length, we need to track
+            # the remaining original gap in the diskmap_table
+            diskmap_table.insert(
+                table_gap_index + 1, (gap_length - file_length, gap_id)
+            )
 
-    checksum = 0
-    curr_idx = 0
     # To calculate the checksum, add up the result of multiplying
     # each of these blocks' position with the file ID number it contains.
     # The leftmost block is in position 0.
     # If a block contains free space, skip it instead.
-    # Example: 009981118882777333...
+    # Example block: 009981118882777333...
     # Represented in diskmap_table like this:
     # # [(2, 0), (2, 9), (1, 8), (3, 1), (3, 8), (1, 2), (3, 7), (3, 3), ...]
+    # 0 * 0 = 0, 1 * 0 = 0, 2 * 9 = 18, 3 * 9 = 27, 4 * 8 = 32
+    checksum = 0
+    block_idx = 0
     for diskmap_tuple in diskmap_table:
         entry_length, entry_id = diskmap_tuple
-        next_idx = curr_idx + entry_length
+        next_entry_block_idx = block_idx + entry_length
         if entry_id == -1:  # Check if this is a gap
-            curr_idx = next_idx
+            block_idx = next_entry_block_idx
             continue
-        while curr_idx < next_idx:
-            checksum += curr_idx * entry_id  # This is a file
-            curr_idx += 1
+        while block_idx < next_entry_block_idx:
+            checksum += block_idx * entry_id  # This is a file
+            block_idx += 1
 
     return checksum
 
