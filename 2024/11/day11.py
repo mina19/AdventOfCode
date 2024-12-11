f7a85650e88731c81f0e573db8393f72c5c661cc
Min A Cho Zeno <minacho19@gmail.com>
2024-12-11 08:20:00 -0600

    Add more docstrings

--- 2024/11/day11.py
+++ 2024/11/day11.py
@@ -6,14 +6,21 @@ from get_data import save_data, timeit
 
 save_data(2024, day := 11)
 data = Path(f"2024/{day}/day{day:02d}.txt").read_text().splitlines()
-# data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()
+data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()
 
 
 ## Part 1
 @timeit
 def part1(n):
+    # [253000, 1, 7]
+    # [253, 0, 2024, 14168]
+    # [512072, 1, 20, 24, 28676032]
+    # [512, 72, 2024, 2, 0, 2, 4, 2867, 6032]
+    # [1036288, 7, 2, 20, 24, 4048, 1, 4048, 8096, 28, 67, 60, 32]
     stones = [int(num) for num in data[0].split()]
     for i in range(n):
+        # Create a new list with all stones, including duplicates
+        # Probably won't scale...
         new_stones = []
         for stone in stones:
             stone_string = str(stone)
@@ -36,10 +43,17 @@ print(part1(25))
 ## Part 2
 @timeit
 def part2(n):
+    # Counter({253000: 1, 1: 1, 7: 1})
+    # Counter({253: 1, 0: 1, 2024: 1, 14168: 1})
+    # Counter({512072: 1, 1: 1, 20: 1, 24: 1, 28676032: 1})
+    # Counter({2: 2, 512: 1, 72: 1, 2024: 1, 0: 1, 4: 1, 2867: 1, 6032: 1})
+    # Counter({4048: 2, 1036288: 1, 7: 1, 2: 1, 20: 1, 24: 1, 1: 1, 8096: 1, 28: 1, 67: 1, 60: 1, 32: 1})
     stones = [int(num) for num in data[0].split()]
     stones = Counter(stones)
 
     for i in range(n):
+        # Only store unique values with their counts
+        # Should scale
         new_stones = Counter()
         for stone, stone_count in stones.items():
             stone_string = str(stone)
