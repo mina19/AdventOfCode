1c58ae009dd95b1753f5d7ab2ff1dda32bcbeba4
Min A Cho Zeno <minacho19@gmail.com>
2024-12-19 22:13:42 -0600

    Remove making register copy since no longer using brute force method for part 2

--- 2024/17/day17.py
+++ 2024/17/day17.py
@@ -9,12 +9,12 @@ save_data(2024, day := 17)
 data = Path(f"2024/{day}/day{day:02d}.txt").read_text()
 # data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text()
 
-register_pattern = "Register (.): (\d+)"
+register_pattern = r"Register (.): (\d+)"
 
-registers_original = {}
+register = {}
 for line in data.split("\n\n")[0].split("\n"):
     match = re.match(register_pattern, line)
-    registers_original[match[1]] = int(match[2])
+    register[match[1]] = int(match[2])
 
 instructions = [
     int(num)
@@ -24,10 +24,9 @@ instructions = [
 
 ## Part 1
 @timeit
-def part1(registers):
+def part1():
     i = 0
     outputs = []
-    registers = registers.copy()
     while i < len(instructions) - 1:
         # Get the literal opcode and operand
         opcode = instructions[i]
@@ -47,37 +46,37 @@ def part1(registers):
             6: "C",
         }
         if 3 < operand < 7:
-            combo_operand = registers[combo_dict[operand]]
+            combo_operand = register[combo_dict[operand]]
 
         if opcode == 0:
             # The adv instruction (opcode 0) performs division.
             # The numerator is the value in the A register.
             # The denominator is found by raising 2 to the power of the
             # instruction's combo operand.
-            registers["A"] = registers["A"] // (1 << combo_operand)
+            register["A"] = register["A"] // (1 << combo_operand)
         elif opcode == 1:
             # The bxl instruction (opcode 1) calculates the bitwise XOR
             # of register B and the instruction's literal operand,
             # then stores the result in register B.
-            registers["B"] = registers["B"] ^ operand
+            register["B"] = register["B"] ^ operand
         elif opcode == 2:
             # The bst instruction (opcode 2) calculates the value of its
             # combo operand modulo 8 (thereby keeping only its lowest 3 bits),
             # then writes that value to the B register.
-            registers["B"] = combo_operand % 8
+            register["B"] = combo_operand % 8
         elif opcode == 3:
             # The jnz instruction (opcode 3) does nothing if the A register
             # is 0. However, if the A register is not zero, it jumps by
             # setting the instruction pointer to the value of its literal operand;
             # if this instruction jumps, the instruction pointer is not increased
             # by 2 after this instruction.
-            if registers["A"] != 0:
+            if register["A"] != 0:
                 i = operand
         elif opcode == 4:
             # The bxc instruction (opcode 4) calculates the bitwise XOR of
             # register B and register C, then stores the result in register B.
             # (For legacy reasons, this instruction reads an operand but ignores it.)
-            registers["B"] = registers["B"] ^ registers["C"]
+            register["B"] = register["B"] ^ register["C"]
         elif opcode == 5:
             # The out instruction (opcode 5) calculates the value of its
             # combo operand modulo 8, then outputs that value.
@@ -87,18 +86,18 @@ def part1(registers):
             # The bdv instruction (opcode 6) works exactly like the adv
             # instruction except that the result is stored in the B register.
             # (The numerator is still read from the A register.)
-            registers["B"] = registers["A"] // (1 << combo_operand)
+            register["B"] = register["A"] // (1 << combo_operand)
         elif opcode == 7:
             # The cdv instruction (opcode 7) works exactly like the adv instruction
             # except that the result is stored in the C register.
             # (The numerator is still read from the A register.)
-            registers["C"] = registers["A"] // (1 << combo_operand)
+            register["C"] = register["A"] // (1 << combo_operand)
 
     # Join the values it output into a single string
     return ",".join(str(x) for x in outputs)
 
 
-print(part1(registers_original))
+print(part1())
 
 
 ## Part 2
