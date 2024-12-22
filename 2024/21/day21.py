b184088344e0241d5113aa042ff25633581f570c
Min A Cho Zeno <minacho19@gmail.com>
2024-12-21 19:01:50 -0600

    Clean up solution

--- 2024/21/day21.py
+++ 2024/21/day21.py
@@ -3,10 +3,11 @@ from pathlib import Path
 from collections import defaultdict, deque
 from get_data import save_data, timeit
 from itertools import product
+from functools import cache
 
 save_data(2024, day := 21)
 data = Path(f"2024/{day}/day{day:02d}.txt").read_text().splitlines()
-data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()
+# data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()
 
 # Information:
 
@@ -71,52 +72,6 @@ movement_dict = {
 }
 
 movement_reverse_dict = {val: key for key, val in movement_dict.items()}
-
-
-# Check robot 1 movements
-def numeric_robot(movements):
-    robot_movements = [char for char in movements]
-
-    robot_row, robot_col = (3, 2)
-    robot_output = ""
-    for movement in robot_movements:
-        drow, dcol = movement_dict[movement]
-        robot_row += drow
-        robot_col += dcol
-        if movement == "A":  # Pushing a button
-            robot_output += numeric_keypad[robot_row][robot_col]
-    return robot_output
-
-
-# Check robot 2, 3 movements
-def directional_robot(movements):
-    robot_movements = [char for char in movements]
-
-    robot_row, robot_col = (0, 2)
-    robot_output = ""
-    for movement in robot_movements:
-        drow, dcol = movement_dict[movement]
-        robot_row += drow
-        robot_col += dcol
-        if movement == "A":  # Pushing a button
-            robot_output += directional_keypad[robot_row][robot_col]
-    return robot_output
-
-
-def check_instructions(instructions, code):
-    robot2_instructions = directional_robot(instructions)
-    robot1_instructions = directional_robot(robot2_instructions)
-    return_code = numeric_robot(robot1_instructions)
-    return return_code == code
-
-
-sample_code_solutions = {
-    "029A": "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A",
-    "980A": "<v<A>>^AAAvA^A<vA<AA>>^AvAA<^A>A<v<A>A>^AAAvA<^A>A<vA>^A<A>A",
-    "179A": "<v<A>>^A<vA<A>>^AAvAA<^A>A<v<A>>^AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-    "456A": "<v<A>>^AA<vA<A>>^AAvAA<^A>A<vA>^A<A>A<vA>^A<A>A<v<A>A>^AAvA<^A>A",
-    "379A": "<v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A",
-}
 
 
 def find_all_shortest_paths(grid, start_pos, end_char):
@@ -192,50 +147,40 @@ def find_directional_robot_instructions(code):
     return instruction_candidates
 
 
-## Part 1
+# Generalized directional robot results
+def direction_robot_instructions(instructions):
+    robot_instructions = []
+    for instruction in instructions:
+        robot_instruction = find_directional_robot_instructions(instruction)
+        robot_instructions += robot_instruction
+
+    min_length = min(len(x) for x in robot_instructions)
+    shortest_robot_instructions = [
+        t for t in robot_instructions if len(t) == min_length
+    ]
+    return shortest_robot_instructions
+
+
 @timeit
-def part1():
+def solve(n):
     result = 0
 
     for code in data:
         # Find what needs to be pressed for robot1
         robot1_instructions = find_numeric_robot_instructions(code)
 
-        robot2_instructions = []
-        for robot1_instruction in robot1_instructions:
-            robot2_instruction = find_directional_robot_instructions(robot1_instruction)
-            robot2_instructions += robot2_instruction
+        instructions = robot1_instructions
+        for i in range(n):
+            instructions = direction_robot_instructions(instructions)
 
-        min_length = min(len(x) for x in robot2_instructions)
-        shortest_robot2_instructions = [
-            t for t in robot2_instructions if len(t) == min_length
-        ]
-
-        robot3_instructions = []
-        for robot2_instruction in shortest_robot2_instructions:
-            robot3_instruction = find_directional_robot_instructions(robot2_instruction)
-            robot3_instructions += robot3_instruction
-
-        min_length = min(len(x) for x in robot3_instructions)
-        shortest_robot3_instructions = [
-            t for t in robot3_instructions if len(t) == min_length
-        ]
-
-        instructions = shortest_robot3_instructions[0]
-
-        if check_instructions(instructions, code):
-            result += len(instructions) * int("".join(filter(str.isdigit, code)))
+        result += len(instructions[0]) * int("".join(filter(str.isdigit, code)))
 
     return result
 
 
-print(part1())
+## Part 1
+print(solve(2))
 
 
 ## Part 2
-@timeit
-def part2():
-    pass
-
-
-part2()
+print(solve(25))
