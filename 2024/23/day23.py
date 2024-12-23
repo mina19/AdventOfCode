e6ea440a3fbca29f8ea9e9c3814bb4afb2ea8dd1
Min A Cho Zeno <minacho19@gmail.com>
2024-12-23 10:01:12 -0600

    Clean up part 1

--- 2024/23/day23.py
+++ 2024/23/day23.py
@@ -1,4 +1,5 @@
 ## Pull Data
+from collections import defaultdict
 from pathlib import Path
 
 from get_data import save_data, timeit
@@ -9,24 +10,35 @@ data = Path(f"2024/{day}/day{day:02d}.txt").read_text().splitlines()
 
 network_pairs = [line.split('-') for line in data]
 
-# All computers in network:
-computers = list(set( [item for sublist in network_pairs for item in sublist]))
-computers_start_t = [comp for comp in computers if comp.startswith('t')]
-
 ## Part 1
 @timeit
 def part1():
-    solutions = []
-    networks_with_computers_start_t = [pair for pair in network_pairs if any(computer in computers_start_t for computer in pair)]
-    for network in networks_with_computers_start_t:
-        (other_comp, t_comp) = (network[1], network[0]) if network[0].startswith('t') else (network[0], network[1])
-        networks_with_other_comp = [pair for pair in network_pairs if other_comp in pair]
-        for network in networks_with_other_comp:
-            third_comp = network[1] if network[0] == other_comp else network[0]
-            if [third_comp, t_comp] in network_pairs or [t_comp, third_comp] in network_pairs:
-                if set([third_comp, other_comp, t_comp]) not in solutions:
-                    solutions.append(set([third_comp, other_comp, t_comp]))
-    return len(solutions)
+   # Find networks that have at least one computer starting with 't'
+   t_networks = [pair for pair in network_pairs 
+                if any(computer.startswith('t') for computer in pair)]
+   
+   solutions = []
+   for network in t_networks:
+       # Identify t_comp and other_comp from the pair
+       t_comp = next(comp for comp in network if comp.startswith('t'))
+       other_comp = next(comp for comp in network if comp != t_comp)
+       
+       # Find all networks containing other_comp
+       connected_networks = [pair for pair in network_pairs if other_comp in pair]
+       
+       # For each network connected to other_comp
+       for connected_net in connected_networks:
+           # Find the third computer
+           third_comp = next(comp for comp in connected_net if comp != other_comp)
+           
+           # Check if third_comp is also connected to t_comp
+           if [third_comp, t_comp] in network_pairs or [t_comp, third_comp] in network_pairs:
+               # Add the trio to solutions if not already present
+               trio = {third_comp, other_comp, t_comp}
+               if trio not in solutions:
+                   solutions.append(trio)
+   
+   return len(solutions)
 
 
 print(part1())
@@ -37,5 +49,4 @@ print(part1())
 def part2():
     pass
 
-
-part2()
+print(part2())
