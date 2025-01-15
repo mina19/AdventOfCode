c51a9834364735a66625eba108967a169210a86e
Min A Cho Zeno <minacho19@gmail.com>
2025-01-14 19:52:40 -0600

    Finished day 22

--- 2024/22/day22.py
+++ 2024/22/day22.py
@@ -1,4 +1,5 @@
 ## Pull Data
+from collections import defaultdict
 from pathlib import Path
 
 from get_data import save_data, timeit
@@ -57,14 +58,30 @@ def part1():
 
     return result
 
-
 print(part1())
-
 
 ## Part 2
 @timeit
 def part2():
-    pass
+    buys = defaultdict(lambda: [0]*len(data))
+    
+    for i, line in enumerate(data):
+        secret_number = int(line)
+        prices = [secret_number % 10]
+        moves = []
+        
+        for j in range(2000):
+            secret_number = iterate(secret_number)
+            prices.append(secret_number % 10)
+            moves.append(prices[-1] - prices[-2])
+            
+            if j >= 3:  # We need 4 moves, and we start collecting after first number
+                last_four_moves = tuple(moves[-4:])
+                if buys[last_four_moves][i] == 0:
+                    buys[last_four_moves][i] = prices[-1]
+    
+    # buys.keys() are the tuples that describe the 4 moves
+    # buys.values() are the number of bananas bought for each buyer
+    return max(sum(price for price in prices) for prices in buys.values())
 
-
-part2()
+print(part2())
