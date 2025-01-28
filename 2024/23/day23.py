bf0d49457fbe1bff16faa07fd79a489a839cd29b
Min A Cho Zeno <minacho19@gmail.com>
2025-01-27 19:21:57 -0600

    Day 23 part 2

--- 2024/23/day23.py
+++ 2024/23/day23.py
@@ -1,4 +1,5 @@
 ## Pull Data
+from collections import defaultdict
 from pathlib import Path
 
 from get_data import save_data, timeit
@@ -8,12 +9,15 @@ data = Path(f"2024/{day}/day{day:02d}.txt").read_text().splitlines()
 # data = Path(f"2024/{day}/day{day:02d}_sample.txt").read_text().splitlines()
 
 network_pairs = [line.split('-') for line in data]
+graph = defaultdict(set)
+computers = set()
 
-# Get all unique computers
-all_computers = set()
+# Parse the input pairs and build the graph
 for comp1, comp2 in network_pairs:
-    all_computers.add(comp1)
-    all_computers.add(comp2)
+    graph[comp1].add(comp2)
+    graph[comp2].add(comp1)
+    computers.add(comp1)
+    computers.add(comp2)
 
 ## Part 1
 @timeit
@@ -51,7 +55,45 @@ print(part1())
 
 ## Part 2
 @timeit
-def part2():
-    pass
+def find_max_clique():
+    def is_clique(nodes):
+        """Check if the given set of nodes forms a clique"""
+        for node in nodes:
+            # For each node, check if it's connected to all other nodes
+            if not all(other_node in graph[node] for other_node in nodes if other_node != node):
+                return False
+        return True
+    
+    def find_all_cliques(current_clique, remaining):
+        """Recursively find all possible cliques"""
+        if not remaining:
+            if is_clique(current_clique):
+                return [current_clique]
+            return []
+        
+        # Track the cliques found
+        cliques = []
+        current_node = remaining[0]
 
-print(part2())
+        # Try including the current node
+        new_clique = current_clique | {current_node}
+        if is_clique(new_clique):
+            cliques.extend(find_all_cliques(new_clique, 
+                                         [node for node in remaining[1:] 
+                                          if node in graph[current_node]]))
+        
+        # Try excluding the current node
+        cliques.extend(find_all_cliques(current_clique, remaining[1:]))
+        
+        return cliques
+
+    # Start with empty clique and all nodes as candidates
+    all_cliques = find_all_cliques(set(), list(computers))
+    
+    # Return the largest clique found
+    if not all_cliques:
+        return set()
+    
+    return ','.join(sorted(max(all_cliques, key=len)))
+
+print(find_max_clique())
