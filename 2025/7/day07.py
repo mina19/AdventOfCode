a3236c8f1377c10764a88cb7a0637f9b1e2f9de7
Min A Cho Zeno <minacho19@gmail.com>
2025-12-07 12:31:39 -0600

    apply ruff

--- 2025/7/day07.py
+++ 2025/7/day07.py
@@ -8,7 +8,7 @@ from pathlib import Path
 year = 2025
 day = 7
 lines = Path(f"{year}/{day}/day{day:02d}.txt").read_text().splitlines()
-lines = Path(f"{year}/{day}/day{day:02d}_sample.txt").read_text().splitlines()
+# lines = Path(f"{year}/{day}/day{day:02d}_sample.txt").read_text().splitlines()
 
 # Shape of data
 rows = len(lines)
@@ -26,27 +26,26 @@ data_dict = defaultdict(
 ## Part 1
 # @timeit
 def part1():
-    start_col = next(col for col in range(cols) if data_dict[0][col] == 'S')
-    
+    start_col = next(col for col in range(cols) if data_dict[0][col] == "S")
+
     beam_cols = {start_col}
     splits = 0
-    
+
     for row in range(rows):
         new_beam_cols = set()
         for beam_col in beam_cols:
             if data_dict[row][beam_col] == "^":
                 splits += 1
-                if data_dict[row+1][beam_col - 1] != "!":
+                if data_dict[row + 1][beam_col - 1] != "!":
                     new_beam_cols.add(beam_col - 1)
-                if data_dict[row+1][beam_col + 1] != "!":
+                if data_dict[row + 1][beam_col + 1] != "!":
                     new_beam_cols.add(beam_col + 1)
             else:
                 new_beam_cols.add(beam_col)
-        
-        beam_cols = new_beam_cols
-    
-    return splits
 
+        beam_cols = new_beam_cols
+
+    return splits
 
 
 print(part1())
@@ -54,9 +53,9 @@ print(part1())
 
 ## Part 2
 # @timeit
-def part2():
-    start_col = next(col for col in range(cols) if data_dict[0][col] == 'S')
-    
+def part2_does_not_scale():
+    start_col = next(col for col in range(cols) if data_dict[0][col] == "S")
+
     # Each beam is now (column, path_tuple)
     beams = {(start_col, ())}
 
@@ -64,21 +63,50 @@ def part2():
         beams2 = set()
         for beam_col, path in beams:
             new_path = path + (beam_col,)
-            
-            if data_dict[row][beam_col] == '^':
+
+            if data_dict[row][beam_col] == "^":
                 beams2.add((beam_col + 1, new_path))
                 beams2.add((beam_col - 1, new_path))
             else:
                 beams2.add((beam_col, new_path))
-        
+
         beams = beams2
-    
+
     # Collect all final timelines
     all_timelines = set()
     for beam_col, path in beams:
         final_path = path + (beam_col,)
         all_timelines.add(final_path)
-    
+
     return len(all_timelines)
 
-print(part2())
+
+def part2_count():
+    start_col = next(col for col in range(cols) if data_dict[0][col] == "S")
+
+    memo = {}
+
+    def count_timelines(row, col):
+        # Count distinct timelines from this position to the end.
+        if (row, col) in memo:
+            return memo[row, col]
+
+        # Base case: reached the last row
+        if row + 1 == rows:
+            return 1
+
+        # Beam continues straight
+        if data_dict[row][col] != "^":
+            memo[row, col] = count_timelines(row + 1, col)
+            return memo[row, col]
+
+        # Split into two timelines
+        left_timelines = count_timelines(row + 1, col - 1)
+        right_timelines = count_timelines(row + 1, col + 1)
+        memo[row, col] = left_timelines + right_timelines
+        return memo[row, col]
+
+    return count_timelines(0, start_col)
+
+
+print(part2_count())
