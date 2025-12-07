a15a37b4b2b0709ae03e1aa65c15e43ae2525c94
Min A Cho Zeno <minacho19@gmail.com>
2025-12-07 12:35:04 -0600

    save

---
+++ 2025/8/day08.py
@@ -0,0 +1,26 @@
+## Pull Data
+from pathlib import Path
+
+from get_data import save_data, timeit
+
+save_data(year := 2025, day := 8)
+data = Path(f"{year}/{day}/day{day:02d}.txt").read_text()
+data = Path(f"{year}/{day}/day{day:02d}_sample.txt").read_text()
+
+
+## Part 1
+@timeit
+def part1():
+    pass
+
+
+part1()
+
+
+## Part 2
+@timeit
+def part2():
+    pass
+
+
+part2()
