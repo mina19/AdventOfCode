88596c9e5e735d85712ff8399af0ee53d62ea761
Min-A Zeno <mczeno@eprod.com>
2025-12-10 11:42:07 -0600

    save

--- 2025/10/day10.py
+++ 2025/10/day10.py
@@ -2,7 +2,12 @@ from itertools import product
 from pathlib import Path
 
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import (
+    Bounds,
+    LinearConstraint,
+    linprog,
+    milp,
+)
 
 from get_data import save_data, timeit
 
@@ -38,7 +43,7 @@ for diagram, schematic in zip(diagrams, button_wiring_schematics):
 
 joltages = [item[-1] for item in split_data]
 joltages = [
-    set(map(int, item.replace("{", "").replace("}", "").split(",")))
+    np.array(list(map(int, item.replace("{", "").replace("}", "").split(","))))
     for item in joltages
 ]
 
@@ -70,8 +75,6 @@ def part1():
 
     return result
 
-    print("hello")
-
 
 print(part1())
 
@@ -79,7 +82,81 @@ print(part1())
 ## Part 2
 @timeit
 def part2():
-    pass
+    # DOES NOT WORK
+    # def solve_buttons(button_schematic, diagram):
+    #     n_buttons = button_schematic.shape[0]
+    #     min_presses = np.inf
+    #     best_solution = None
+
+    #     # Try all possible combinations of button presses (0 or 1)
+    #     for x in product([0, 1], repeat=n_buttons):
+    #         x_vec = np.array(x)
+    #         # Check if the combination matches the diagram
+    #         if np.array_equal((button_schematic.T @ x_vec), diagram):
+    #             presses = np.sum(x_vec)
+    #             if presses < min_presses:
+    #                 min_presses = presses
+    #                 best_solution = x_vec
+
+    #     return best_solution, min_presses
+
+    # def solve_buttons(button_schematic, diagram):
+    #     n_buttons = button_schematic.shape[0]
+
+    #     c = np.ones(n_buttons)
+
+    #     # Constraints: button_schematic.T @ x == diagram
+    #     A_eq = button_schematic.T
+    #     b_eq = diagram
+
+    #     # Bounds: x >= 0, integer
+    #     max_press = sum(diagram) * 1000
+    #     bounds = [(0, np.inf)] * n_buttons
+
+    #     # Solve as a continuous problem first
+    #     res = linprog(c, A_eq=A_eq, b_eq=b_eq, bounds=bounds, method="simplex")
+
+    #     if res.success:
+    #         # Round to nearest integer and check if it matches
+    #         x_sol = np.round(res.x).astype(int)
+    #         if np.array_equal(A_eq @ x_sol, b_eq):
+    #             return x_sol, np.sum(x_sol)
+
+    #     return None, 0
+
+    def solve_buttons(button_schematic, diagram, max_press=1000):
+        n_buttons = button_schematic.shape[0]
+
+        # Minimize total button presses
+        c = np.ones(n_buttons)
+        A_eq = button_schematic.T
+        b_eq = diagram
+        bounds = Bounds(0, max_press)
+        integrality = np.full(n_buttons, 1)
+        constraint = LinearConstraint(A_eq, b_eq, b_eq)
+
+        res = milp(
+            c=c,
+            constraints=constraint,
+            bounds=bounds,
+            integrality=integrality,
+            options={"disp": False},
+        )
+
+        if res.success:
+            x_sol = np.round(res.x).astype(int)
+            if np.array_equal(A_eq @ x_sol, b_eq):
+                return x_sol, np.sum(x_sol)
+
+        return None, 0
+
+    result = 0
+    for joltage, button_schematic in zip(joltages, button_schematics):
+        res, num_presses = solve_buttons(np.array(button_schematic), joltage)
+        print(num_presses)
+        result += num_presses
+
+    return result
 
 
-part2()
+print(part2())
