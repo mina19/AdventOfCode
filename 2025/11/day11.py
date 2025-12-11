67d11219c27639f3b3eb1ee1d9ca63f35e11f898
Min-A Zeno <mczeno@eprod.com>
2025-12-11 08:37:42 -0600

    save work so far

---
+++ 2025/11/day11.py
@@ -0,0 +1,65 @@
+## Pull Data
+from pathlib import Path
+
+from get_data import save_data, timeit
+
+save_data(year := 2025, day := 11)
+data = Path(f"{year}/{day}/day{day:02d}.txt").read_text().splitlines()
+# data = Path(f"{year}/{day}/day{day:02d}_sample.txt").read_text().splitlines()
+
+devices = [line.split(":")[0] for line in data]
+outputs = [line.split(":")[1].split(" ")[1:] for line in data]
+data_dict = dict(zip(devices, outputs))
+
+
+## Part 1
+@timeit
+def part1():
+    start_device = next(device for device in devices if device == "you")
+    paths = [[start_device]]
+    all_paths = []
+
+    while paths:
+        new_paths = []
+        for path in paths:
+            last_device = path[-1]
+            if last_device == "out":
+                all_paths.append(path)
+                continue
+            for future_device in data_dict[last_device]:
+                if future_device not in path:
+                    new_paths.append(path + [future_device])
+        paths = new_paths
+
+    return len(all_paths)
+
+
+# print(part1())
+
+
+## Part 2
+@timeit
+def mini_solver(start_device, required_device):
+    start_device = next(device for device in devices if device == start_device)
+    paths = [[start_device]]
+    all_paths = []
+
+    while paths:
+        new_paths = []
+        for path in paths:
+            last_device = path[-1]
+            if last_device == "out":
+                all_paths.append(path)
+                continue
+            for future_device in data_dict[last_device]:
+                if future_device not in path:
+                    new_paths.append(path + [future_device])
+        paths = new_paths
+
+    valid_paths = [path for path in all_paths if required_device in path]
+
+    return len(valid_paths)
+
+
+print(mini_solver("dac", "fft"))
+print(mini_solver("fft", "dac"))
